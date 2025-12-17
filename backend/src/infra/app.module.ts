3b0dc4909d0608308c238d5d025746353ed25131
MarcosPaul0 <marcosphip7@gmail.com>
2025-12-16 22:05:30 -0300

    fix: jwt cookie type

--- backend/src/infra/app.module.ts
+++ backend/src/infra/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env/env'
-import { AuthModule } from './auth/auth.module'
-import { HttpModule } from './http/http.module'
-import { EnvModule } from './env/env.module'
-import { CronModule } from '@/jobs/cron/cron.module'
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env/env";
+import { AuthModule } from "./auth/auth.module";
+import { HttpModule } from "./http/http.module";
+import { EnvModule } from "./env/env.module";
+import { CronModule } from "@/jobs/cron/cron.module";
 
 @Module({
   imports: [
--- backend/src/infra/cryptography/jwt-encrypter.ts
+++ backend/src/infra/cryptography/jwt-encrypter.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { EnvService } from '../env/env.service';
-import { TokenEncrypter } from '@/domain/application/cryptography/token-encrypter';
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { EnvService } from "../env/env.service";
+import { TokenEncrypter } from "@/domain/application/cryptography/token-encrypter";
 
 @Injectable()
 export class JwtEncrypter implements TokenEncrypter {
   constructor(
     private jwtService: JwtService,
-    private envService: EnvService,
+    private envService: EnvService
   ) {}
 
   generateAccessToken(payload: Record<string, unknown>): Promise<string> {
     const expiresIn = this.envService.get(
-      'JWT_ACCESS_TOKEN_EXPIRATION_SECONDS',
+      "JWT_ACCESS_TOKEN_EXPIRATION_SECONDS"
     );
     return this.jwtService.signAsync(payload, {
       expiresIn,
@@ -20,7 +20,8 @@ export class JwtEncrypter implements TokenEncrypter {
   }
 
   generateRefreshToken(payload: Record<string, unknown>): Promise<string> {
-    const expiresIn = `${this.envService.get('JWT_REFRESH_TOKEN_EXPIRATION_DAYS')}d`;
+    const expiresIn =
+      `${this.envService.get("JWT_REFRESH_TOKEN_EXPIRATION_DAYS")}d` as unknown as number;
     return this.jwtService.signAsync(payload, {
       expiresIn,
     });
