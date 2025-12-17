5185eae389db2ac72a4142b27c9df29923aee21b
MarcosPaul0 <marcosphip7@gmail.com>
2025-12-16 22:04:49 -0300

    fix: cookie type

--- backend/src/infra/http/controllers/authenticate/authenticate.controller.ts
+++ backend/src/infra/http/controllers/authenticate/authenticate.controller.ts
@@ -6,14 +6,14 @@ import {
   Res,
   UnauthorizedException,
   UsePipes,
-} from '@nestjs/common';
-import { ZodValidationPipe } from '../../pipes/zod-validation-pipe';
-import { z } from 'zod';
-import { AuthenticateUserUseCase } from '@/domain/application/use-cases/authenticate-user/authenticate-user';
-import { WrongCredentialsError } from '@/domain/application/use-cases/errors/wrong-credentials-error';
-import { Public } from '@/infra/auth/public';
-import { Response } from 'express';
-import { EnvService } from '@/infra/env/env.service';
+} from "@nestjs/common";
+import { ZodValidationPipe } from "../../pipes/zod-validation-pipe";
+import { z } from "zod";
+import { AuthenticateUserUseCase } from "@/domain/application/use-cases/authenticate-user/authenticate-user";
+import { WrongCredentialsError } from "@/domain/application/use-cases/errors/wrong-credentials-error";
+import { Public } from "@/infra/auth/public";
+import { Response } from "express";
+import { EnvService } from "@/infra/env/env.service";
 
 const authenticateBodySchema = z.object({
   email: z.email(),
@@ -22,19 +22,19 @@ const authenticateBodySchema = z.object({
 
 type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
 
-@Controller('/sessions')
+@Controller("/sessions")
 @Public()
 export class AuthenticateController {
   constructor(
     private authenticateUser: AuthenticateUserUseCase,
-    private envService: EnvService,
+    private envService: EnvService
   ) {}
 
   @Post()
   @UsePipes(new ZodValidationPipe(authenticateBodySchema))
   async handle(
     @Body() body: AuthenticateBodySchema,
-    @Res({ passthrough: true }) response: Response,
+    @Res({ passthrough: true }) response: Response
   ) {
     const { email, password } = body;
 
@@ -56,8 +56,10 @@ export class AuthenticateController {
 
     const { accessToken, refreshToken } = result.value;
 
-    response.cookie(this.envService.get('REFRESH_TOKEN_COOKIE'), refreshToken, {
-      path: '/',
+    const refreshTokenCookie = this.envService.get("REFRESH_TOKEN_COOKIE");
+
+    response.cookie(refreshTokenCookie, refreshToken, {
+      path: "/",
       secure: true,
       sameSite: true,
       httpOnly: true,
