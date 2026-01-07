8077a81c108257489c110721fbf7874a6c988a3a
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-06 21:47:05 -0300

    refactor: format files

--- backend/src/infra/http/controllers/find-all-cities/find-all-cities.controller.ts
+++ backend/src/infra/http/controllers/find-all-cities/find-all-cities.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, HttpCode, Param, Query } from '@nestjs/common';
-import { CityPresenter } from '../../presenters/city-presenter';
-import { Public } from '@/infra/auth/public';
-import { FindAllCitiesUseCase } from '@/domain/application/use-cases/find-all-cities/find-all-cities';
-import z from 'zod';
-import { ZodValidationPipe } from '../../pipes/zod-validation-pipe';
+import { Controller, Get, HttpCode, Param, Query } from "@nestjs/common";
+import { CityPresenter } from "../../presenters/city-presenter";
+import { Public } from "@/infra/auth/public";
+import { FindAllCitiesUseCase } from "@/domain/application/use-cases/find-all-cities/find-all-cities";
+import z from "zod";
+import { ZodValidationPipe } from "../../pipes/zod-validation-pipe";
 
 const findAllCitiesQuerySchema = z
   .object({
@@ -13,7 +13,7 @@ const findAllCitiesQuerySchema = z
 
 type FindAllCitiesQuerySchema = z.infer<typeof findAllCitiesQuerySchema>;
 
-@Controller('/cities')
+@Controller("/cities")
 export class FindAllCitiesController {
   constructor(private findAllCitiesUseCase: FindAllCitiesUseCase) {}
 
@@ -22,7 +22,7 @@ export class FindAllCitiesController {
   @HttpCode(200)
   async handle(
     @Query(new ZodValidationPipe(findAllCitiesQuerySchema))
-    query?: FindAllCitiesQuerySchema,
+    query?: FindAllCitiesQuerySchema
   ) {
     const result = await this.findAllCitiesUseCase.execute({
       filters: {
