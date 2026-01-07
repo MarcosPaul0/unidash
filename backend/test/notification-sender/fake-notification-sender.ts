a5e63c9db23cfad02cdccd1d136f3178ce688319
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-06 21:47:20 -0300

    feat: implements fake methods

--- backend/test/notification-sender/fake-notification-sender.ts
+++ backend/test/notification-sender/fake-notification-sender.ts
@@ -1,24 +1,31 @@
 import {
   NotificationSender,
   SendAccountActivationNotificationParams,
+  SendIncomingStudentRegistrationNotificationParams,
   SendPasswordResetNotificationParams,
-} from '@/domain/application/notification-sender/notification-sender';
+} from "@/domain/application/notification-sender/notification-sender";
 
 export class FakeNotificationSender implements NotificationSender {
   async sendAccountActivationNotification(
-    params: SendAccountActivationNotificationParams,
+    params: SendAccountActivationNotificationParams
   ): Promise<void> {
     void params;
   }
 
   async sendPasswordResetNotification(
-    params: SendPasswordResetNotificationParams,
+    params: SendPasswordResetNotificationParams
+  ): Promise<void> {
+    void params;
+  }
+
+  async sendIncomingStudentRegistrationNotification(
+    params: SendIncomingStudentRegistrationNotificationParams
   ): Promise<void> {
     void params;
   }
 
   async sendEmailConfirmationReminderNotification(
-    params: SendAccountActivationNotificationParams,
+    params: SendAccountActivationNotificationParams
   ): Promise<void> {
     void params;
   }
