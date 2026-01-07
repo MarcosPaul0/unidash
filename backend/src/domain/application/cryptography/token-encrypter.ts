e53c1fd6b00d3051910223433ba50ff19c795f48
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-06 21:37:30 -0300

    feat: adds incoming student token methods

--- backend/src/domain/application/cryptography/token-encrypter.ts
+++ backend/src/domain/application/cryptography/token-encrypter.ts
@@ -1,11 +1,16 @@
 export abstract class TokenEncrypter {
   abstract generateAccessToken(
-    payload: Record<string, unknown>,
-  ): Promise<string>
-
+    payload: Record<string, unknown>
+  ): Promise<string>;
   abstract generateRefreshToken(
-    payload: Record<string, unknown>,
-  ): Promise<string>
+    payload: Record<string, unknown>
+  ): Promise<string>;
+  abstract verifyToken(token: string): Promise<Record<string, unknown> | null>;
 
-  abstract verifyToken(token: string): Promise<Record<string, unknown> | null>
+  abstract generateIncomingStudentToken(
+    payload: Record<string, unknown>
+  ): Promise<string>;
+  abstract verifyIncomingStudentToken(
+    token: string
+  ): Promise<Record<string, unknown> | null>;
 }
