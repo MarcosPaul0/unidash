79010f5b7d2bbd14e50b4ef67a34dc67565edb19
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-14 20:19:38 -0300

    feat: add regular student type label

--- frontend/src/app/(private)/student/_components/StudentType/index.tsx
+++ frontend/src/app/(private)/student/_components/StudentType/index.tsx
@@ -5,11 +5,13 @@ import { STUDENT_TYPE } from "@unidash/api/responses/student.response";
 const STUDENT_TYPE_COLOR = {
   [STUDENT_TYPE.incomingStudent]: "bg-icon-orange text-icon-orange-foreground",
   [STUDENT_TYPE.outgoingStudent]: "bg-icon-purple text-icon-purple-foreground",
+  [STUDENT_TYPE.regularStudent]: "bg-icon-blue text-icon-blue-foreground",
 } as const;
 
 const STUDENT_TYPE_LABEL = {
   [STUDENT_TYPE.incomingStudent]: "Ingressante",
   [STUDENT_TYPE.outgoingStudent]: "Egressante",
+  [STUDENT_TYPE.regularStudent]: "Regular",
 } as const;
 
 export function StudentType({ type }: StudentTypeProps) {
