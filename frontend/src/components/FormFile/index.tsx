8c867c834c36ada770ec81b2c297afd0c1501a80
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-19 20:54:34 -0300

    feat: implements form file

--- frontend/src/components/FormFile/index.tsx
+++ frontend/src/components/FormFile/index.tsx
@@ -1,25 +1,30 @@
-import { UploadSimpleIcon } from "@phosphor-icons/react/dist/ssr";
+import { FilePdfIcon, UploadSimpleIcon } from "@phosphor-icons/react/dist/ssr";
 import { Card, CardContent, CardFooter } from "../Card";
-import { Button } from "../Button";
 import { FormFileProps } from "./formFile.interface";
+import { useDropzone } from "react-dropzone";
 
-export function FormFile({ helper, label }: FormFileProps) {
+export function FormFile({ helper, label, ...rest }: FormFileProps) {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone(rest);
+
   return (
     <Card
       className={`
         flex flex-col py-4 border 
         border-dashed gap-1 border-muted rounded-2xl
-        pb-1
+        pb-1 ${isDragActive && "border-solid border-primary bg-primary/10"}
       `}
+      {...getRootProps()}
     >
+      <input type="file" {...getInputProps()} hidden />
+
       <CardContent className="flex flex-col items-center">
-        <UploadSimpleIcon size={62} />
+        {isDragActive ? (
+          <FilePdfIcon size={62} />
+        ) : (
+          <UploadSimpleIcon size={62} />
+        )}
 
-        <span>{label}</span>
-
-        <Button variant="link" type="button">
-          Clique para baixar um template
-        </Button>
+        {label}
       </CardContent>
 
       <CardFooter>{helper}</CardFooter>
