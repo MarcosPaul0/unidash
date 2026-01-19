cd3c7f2a03d6d869a1bc3f5fdcdba1c45b5425ad
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-19 20:54:09 -0300

    refactor: update file input props

--- frontend/src/components/FormFile/formFile.interface.ts
+++ frontend/src/components/FormFile/formFile.interface.ts
@@ -1,7 +1,9 @@
+import { ReactNode } from "react";
+import { DropzoneOptions } from "react-dropzone";
 import { Control } from "react-hook-form";
 
-export interface FormFileProps {
-  label?: string;
+export interface FormFileProps extends DropzoneOptions {
+  label: ReactNode;
   helper?: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   control: Control<any, any, any>;
